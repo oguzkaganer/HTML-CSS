c4ba33a35f22eab6432ed439e0965fbab78dcbb1
Oğuz Kağan ER <oguzkaganerr@gmail.com>
2022-12-19 13:35:42 +0300

    update-2 SliderExp

--- SliderExp/script.js
+++ SliderExp/script.js
@@ -1,3 +1,4 @@
+
 var imgs = document.getElementsByTagName("img");
 var i = 0;
 function slideRight() {
@@ -7,6 +8,23 @@ function slideRight() {
     imgs[i + 1].classList.remove("img-right");
     imgs[i + 1].classList.add("img-on");
     i++;
+  } else {
+    imgs[0].classList.remove("img-left");
+    imgs[0].classList.remove("img-right");
+    imgs[0].classList.add("img-on");
+
+    imgs[1].classList.remove("img-on");
+    imgs[1].classList.remove("img-left");
+    imgs[1].classList.add("img-right");
+
+    imgs[2].classList.remove("img-on");
+    imgs[2].classList.remove("img-left");
+    imgs[2].classList.add("img-right");
+
+    imgs[3].classList.remove("img-on");
+    imgs[3].classList.remove("img-left");
+    imgs[3].classList.add("img-right");
+    i=0;
   }
   console.log(i);
 }
@@ -20,6 +38,15 @@ function slideLeft() {
     i--;
   }
   console.log(i);
+}
+
+var points = document.getElementById("points");
+for (var j = 0; j < imgs.length; j++) {
+  var point = document.createElement("div");
+  //point.classList.add("point");
+  point.setAttribute("class","point");
+  point.setAttribute("onclick", "goPage(" + j + ")");
+  points.appendChild(point);
 }
 
 function goPage(index) {
